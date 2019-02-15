4cef0548cbdbd109fc86ed235187caa846804170
COVERT NINE <tim@covertnine.com>
2019-02-15 17:03:40 -0600

    adding responsive containers and testing for mobile layouts of default gutenberg elements and containers

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -9,23 +9,26 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 	<header class="entry-header">
-
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
+		<div class="container">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</div>
 	</header><!-- .entry-header -->
 
 	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
 	<div class="entry-content">
 
-		<?php the_content(); ?>
+		<div class="container">
 
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-			'after'  => '</div>',
-		) );
-		?>
+			<?php the_content(); ?>
+
+			<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			) );
+			?>
+		</div><!-- .container-->
 
 	</div><!-- .entry-content -->
 
