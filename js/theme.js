3506e660baf586c85ecb837643c68019f05a9983
COVERT NINE <tim@covertnine.com>
2019-02-20 12:21:11 -0600

    updating default template to not have page title, navigation javascript working, finished alignments and base style sheet for gutenberg

--- js/theme.js
+++ js/theme.js
@@ -3958,27 +3958,58 @@
 	}
 })();
 
-	jQuery(window).scroll(function () {
-
-	    var scroll = jQuery(window).scrollTop();
-	    var screenWidth = jQuery(window).width();
-
-	    if (scroll >= 168) {
-		    jQuery('.navbar').addClass('navbar-small fixed-top');
-		    jQuery('.header-navbar').addClass('jumpfix');
-	    }
-	    if ((scroll <= 167) && (screenWidth >= 668)) {
-		    jQuery('.navbar').removeClass('navbar-small fixed-top');
-		    jQuery('.header-navbar').removeClass('jumpfix');
-	    }
-
-	});
-
 	if (jQuery(window).width() <= 667) {
 
+		//use small nav for mobile
 		jQuery('.navbar').addClass('navbar-small');
 
 		// add class to top for mobile
 		jQuery('.navbar-top').addClass('navbar-small-mobile');
 
-	} //end small screens
+		jQuery(window).scroll(function () {
+
+			//scroll position variable
+			var scroll = jQuery(window).scrollTop();
+
+			if (scroll >=168) {
+				jQuery('.navbar').addClass('opacity0');
+			}
+			if (scroll <=167) {
+				jQuery('.navbar').removeClass('opacity0');
+			}
+
+		    if (scroll >= 218) {
+			    jQuery('.navbar').addClass('fixed-top opacity100');
+			    jQuery('.header-navbar').addClass('jumpfix'); //accounts for position-fixed CSS change
+		    }
+		    if (scroll <= 217) {
+			    jQuery('.navbar').removeClass('fixed-top opacity100');
+			    jQuery('.header-navbar').removeClass('jumpfix'); //remove extra classes and put navs back at top
+		    }
+		});
+
+	} else { //end small screens
+
+		jQuery(window).scroll(function () {
+
+		    var scroll = jQuery(window).scrollTop();
+		    if (scroll >=168) {
+				jQuery('.navbar').addClass('opacity0');
+			}
+			if (scroll <=167) {
+				jQuery('.navbar').removeClass('opacity0');
+			}
+
+
+		    if (scroll >= 218) {
+			    jQuery('.navbar').addClass('navbar-small fixed-top opacity100'); //shrink nav and fix it to top
+			    jQuery('.header-navbar').addClass('jumpfix');
+		    }
+		    if (scroll <= 217) {
+			    jQuery('.navbar').removeClass('navbar-small fixed-top opacity100'); //expand nav and remove fixed
+			    jQuery('.header-navbar').removeClass('jumpfix');
+		    }
+
+		});
+
+	} //end regular
--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -8,16 +8,6 @@
 ?>
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<header class="entry-header">
-		<div class="container">
-			<div class="row">
-				<div class="col">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-				</div>
-			</div>
-		</div>
-	</header><!-- .entry-header -->
-
 	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
 	<div class="entry-content">
