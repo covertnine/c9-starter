6f5be15f677a09df7d192f5152ab9c465e4d82e0
samirillion <sam@protonmail.com>
2019-07-16 10:02:34 -0500

    fixing a few more things

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,72 +1,101 @@
-import "smoothState";
-import "magnific-popup";
+import "smoothstate";
 
 jQuery(document).ready(function() {
   (function($) {
-    jQuery(window).scroll(function() {
-      //scroll position variable
-      var scroll = jQuery(window).scrollTop();
 
+    var options = {
+      prefetch: true,
+      cacheLength: 2,
+      blacklist: '.mega-menu-item-has-children .mega-menu-link',
+      onStart: {
+        duration: 250, // Duration of our animation
+        render: function ($container) {
+          // Add your CSS animation reversing class
+          $container.addClass('is-exiting');
+  
+          // Restart your animation
+          smoothState.restartCSSAnimations();
+        }
+      },
+      onReady: {
+        duration: 0,
+        render: function ($container, $newContent) {
+          // Remove your CSS animation reversing class
+          $container.removeClass('is-exiting');
+  
+          // Inject the new content
+          $container.html($newContent);
+  
+        }
+      }
+    },
+    smoothState = $('#page').smoothState(options).data('smoothState');
+
+    $(window).scroll(function() {
+      //scroll position variablem
+      var scroll = $(window).scrollTop();
       if (scroll >= 633) {
-        jQuery("#left-sidebar").addClass("fixed-sidebar");
-        jQuery("#right-sidebar").addClass("fixed-sidebar");
+        $("#left-sidebar").addClass("fixed-sidebar");
+        $("#right-sidebar").addClass("fixed-sidebar");
       }
       if (scroll <= 632) {
-        jQuery("#left-sidebar").removeClass("fixed-sidebar");
-        jQuery("#right-sidebar").removeClass("fixed-sidebar");
+        $("#left-sidebar").removeClass("fixed-sidebar");
+        $("#right-sidebar").removeClass("fixed-sidebar");
       }
     });
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ///////////////////////// Mobile and desktop navigation classes //////////////////////////////////////////////////
-    if (jQuery(window).width() <= 667) {
+    if ($(window).width() <= 667) {
       // var Parallax = require('parallax-js')
       // var scene = document.getElementById('wrapper-footer-full');
       // var parallaxInstance = new Parallax(scene);
 
-      //use small nav for mobile
-      jQuery(".navbar").addClass("navbar-small");
+      console.log("sick");
 
-      jQuery(window).scroll(function() {
+      //use small nav for mobile
+      $(".navbar").addClass("navbar-small");
+
+      $(window).scroll(function() {
         //scroll position variable
-        var scroll = jQuery(window).scrollTop();
+        var scroll = $(window).scrollTop();
 
         if (scroll >= 168) {
-          jQuery(".navbar").addClass("opacity0");
+          $(".navbar").addClass("opacity0");
         }
         if (scroll <= 167) {
-          jQuery(".navbar").removeClass("opacity0");
+          $(".navbar").removeClass("opacity0");
         }
 
         if (scroll >= 218) {
-          jQuery(".navbar").addClass("fixed-top opacity100");
-          jQuery(".header-navbar").addClass("jumpfix"); //accounts for position-fixed CSS change
+          $(".navbar").addClass("fixed-top opacity100");
+          $(".header-navbar").addClass("jumpfix"); //accounts for position-fixed CSS change
         }
         if (scroll <= 217) {
-          jQuery(".navbar").removeClass("fixed-top opacity100");
-          jQuery(".header-navbar").removeClass("jumpfix"); //remove extra classes and put navs back at top
+          $(".navbar").removeClass("fixed-top opacity100");
+          $(".header-navbar").removeClass("jumpfix"); //remove extra classes and put navs back at top
         }
       });
     } else {
       //end small screens so desktop next
 
-      jQuery(window).scroll(function() {
+      $(window).scroll(function() {
         //scroll position variable
-        var scroll = jQuery(window).scrollTop();
+        var scroll = $(window).scrollTop();
 
         if (scroll >= 168) {
-          jQuery(".navbar").addClass("opacity0");
+          $(".navbar").addClass("opacity0");
         }
         if (scroll <= 167) {
-          jQuery(".navbar").removeClass("opacity0");
+          $(".navbar").removeClass("opacity0");
         }
 
         if (scroll >= 218) {
-          jQuery(".navbar").addClass("navbar-small fixed-top opacity100"); //shrink nav and fix it to top
-          jQuery(".header-navbar").addClass("jumpfix");
+          $(".navbar").addClass("navbar-small fixed-top opacity100"); //shrink nav and fix it to top
+          $(".header-navbar").addClass("jumpfix");
         }
         if (scroll <= 217) {
-          jQuery(".navbar").removeClass("navbar-small fixed-top opacity100"); //expand nav and remove fixed
-          jQuery(".header-navbar").removeClass("jumpfix");
+          $(".navbar").removeClass("navbar-small fixed-top opacity100"); //expand nav and remove fixed
+          $(".header-navbar").removeClass("jumpfix");
         }
       });
     } //end regular
@@ -75,22 +104,20 @@ jQuery(document).ready(function() {
     ///////////////////////// Move the content up by the height of the navbar object for a transparent nav effect ////
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     if ($(window).width() > 667) {
-      //   var navHeight = $(".header-navbar").height();
-      //   $("#page-wrapper").css("margin-top", -navHeight);
+        // var navHeight = $(".header-navbar").height();
+        // $("#page-wrapper").css("margin-top", -navHeight);
     }
 
     ///////////////////////// for putting wordpress galleries linked to images/videos in lightbox ////////////////////
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    if ($(".cortex-popup-video").length) {
-      $(".cortex-popup-video").magnificPopup({
-        disableOn: 700,
-        type: "iframe",
-        mainClass: "mfp-zoom-in",
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false
-      });
-    }
+    $(".cortex-popup-video").magnificPopup({
+      disableOn: 700,
+      type: "iframe",
+      mainClass: "mfp-zoom-in",
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false
+    });
 
     $(
       '.wp-block-gallery a[href$=".jpg"], .wp-block-gallery a[href$=".jpeg"], .wp-block-gallery a[href$=".png"], .wp-block-gallery a[href$=".gif, "], .cortex-popup'
@@ -203,20 +230,7 @@ jQuery(document).ready(function() {
         }
       }
     );
-
-    // sidebar functionality
-    jQuery(window).scroll(function() {
-      //scroll position variable
-      var scroll = jQuery(window).scrollTop();
-
-      if (scroll >= 633) {
-        jQuery("#left-sidebar").addClass("fixed-sidebar");
-        jQuery("#right-sidebar").addClass("fixed-sidebar");
-      }
-      if (scroll <= 632) {
-        jQuery("#left-sidebar").removeClass("fixed-sidebar");
-        jQuery("#right-sidebar").removeClass("fixed-sidebar");
-      }
-    });
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   })(jQuery);
 });
