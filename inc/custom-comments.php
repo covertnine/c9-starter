080a9a4d29d668f18bd493ec6b48f437246dadd0
samirillion <sam@protonmail.com>
2019-09-11 16:07:30 -0500

    fixing some formatting

--- inc/custom-comments.php
+++ inc/custom-comments.php
@@ -24,20 +24,11 @@ if ( ! function_exists( 'c9_bootstrap_comment_form_fields' ) ) {
 		$aria_req  = ( $req ? " aria-required='true'" : '' );
 		$html5     = current_theme_supports( 'html5', 'comment-form' ) ? 1 : 0;
 		$fields    = array(
-			'author' => '<div class="form-group comment-form-author"><label for="author">' . __(
-				 'Name',
-					'c9'
-				) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
+			'author' => '<div class="form-group comment-form-author"><label for="author">' . __( 'Name', 'c9' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
 						'<input class="form-control" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . '></div>',
-			'email'  => '<div class="form-group comment-form-email"><label for="email">' . __(
-				 'Email',
-					'c9'
-				) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
+			'email'  => '<div class="form-group comment-form-email"><label for="email">' . __( 'Email', 'c9' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
 						'<input class="form-control" id="email" name="email" ' . ( $html5 ? 'type="email"' : 'type="text"' ) . ' value="' . esc_attr( $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . '></div>',
-			'url'    => '<div class="form-group comment-form-url"><label for="url">' . __(
-				 'Website',
-					'c9'
-				) . '</label> ' .
+			'url'    => '<div class="form-group comment-form-url"><label for="url">' . __( 'Website', 'c9' ) . '</label> ' .
 						'<input class="form-control" id="url" name="url" ' . ( $html5 ? 'type="url"' : 'type="text"' ) . ' value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30"></div>',
 		);
 
