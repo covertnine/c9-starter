add2ee539c952d884124d31115b460c4e18af127
COVERT NINE <tim@covertnine.com>
2019-09-16 23:40:26 -0500

    fixed default posts page listing grid

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ get_header();
 
 	<main class="site-main" id="main">
 		<div class="container container-posts c9">
+			<div class="row no-gutter">
 			<?php if ( have_posts() ) : ?>
 
 				<?php /* Start the Loop */ ?>
@@ -44,7 +45,8 @@ the_post();
 				<?php get_template_part( 'loop-templates/content', 'none' ); ?>
 
 			<?php endif; ?>
-		</div><!-- .container-narrow-->
+			</div><!--end .row-->
+		</div><!-- .container-->
 
 	</main><!-- #main -->
 
