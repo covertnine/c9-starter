77516ef7fcd13f790fd548be76cc33378fe98856
samirillion <sam@protonmail.com>
2019-10-16 16:06:49 -0500

    fixing custom font styles

--- assets/fonts/class-c9fontstyles.php
+++ assets/fonts/class-c9fontstyles.php
@@ -9,8 +9,7 @@ class C9FontStyles {
 	 * Gets font settings based on defaults and generates code.-arrow-up
 	 */
 	public static function render( $font_array ) {
-		$heading_font    = $font_array;
-		$headings_font   = "'" . $font_array['heading_font'] . "'";
+		$heading_font    = "'" . $font_array['heading_font'] . "'";
 		$subheading_font = "'" . $font_array['subheading_font'] . "'";
 		$body_font       = "'" . $font_array['body_font'] . "'";
 		?>
@@ -38,17 +37,14 @@ class C9FontStyles {
 		.editor-styles-wrapper h3,
 		.editor-styles-wrapper h4,
 		.editor-styles-wrapper h5,
-		.editor-styles-wrapper h6
-		{
-		font-family: <?php echo $headings_font; ?>;
-		}
-
+		.editor-styles-wrapper h6,
+		.editor-styles-wrapper blockquote:before,
+		.entry-content blockquote:before,
+		.editor-styles-wrapper blockquote:before,
+		.entry-content blockquote:before,
 		.navbar,
-		.navbar ul li .dropdown-item, .navbar ul li a {
-		font-family: <?php echo $headings_font; ?>;
-		}
-
-		@media only screen and (max-width: 768px) {
+		.navbar ul li .dropdown-item,
+		.navbar ul li a,
 		.c9-h,
 		.c9-h.h,
 		.c9-txl,
@@ -57,8 +53,19 @@ class C9FontStyles {
 		.display-3,
 		.display-4,
 		.display-5,
-		.display-6 {
-		font-family: <?php echo $headings_font; ?>;
+		.display-6,
+		.editor-styles-wrapper .c9-cta .c9-h p,
+		.header-navbar .navbar .nav .nav-item .nav-link,
+		.header-navbar .navbar .nav .nav-item .dropdown-item,
+		.header-navbar .navbar .nav .search,
+		.page-search-results nav .pagination .page-item .page-link,
+		.theme-c9.woocommerce nav.woocommerce-pagination ul li span,
+		.theme-c9.woocommerce nav.woocommerce-pagination ul li .page-numbers,
+		.archive nav .pagination .page-item .page-link,
+		.blog nav .pagination .page-item .page-link,
+		.single .navigation .nav-previous a,
+		.single .navigation .nav-next a {
+		font-family: <?php echo $heading_font; ?>;
 		}
 
 		p.wp-block-subhead,
@@ -66,10 +73,7 @@ class C9FontStyles {
 		.c9-sh,
 		.text-muted,
 		.c9 .c9-sh,
-		.c9 .text-muted {
-		font-family: <?php echo $body_font; ?>;
-		}
-
+		.c9 .text-muted,
 		.editor-styles-wrapper p.wp-block-subhead,
 		.editor-styles-wrapper .subhead-h,
 		.editor-styles-wrapper .c9-sh,
@@ -79,9 +83,49 @@ class C9FontStyles {
 		.editor-styles-wrapper .c9-heading h4.subhead-h4,
 		.editor-styles-wrapper .c9-heading h5.subhead-h5,
 		.editor-styles-wrapper .c9-heading h6.subhead-h6 {
-		font-family: <?php echo $body_font; ?>;
+			font-family: <?php echo $subheading_font; ?>;
 		}
 
+		body,
+		.wp-block-pullquote,
+		.wp-block-pullquote blockquote p,
+		#wrapper-footer,
+		p.wp-block-subhead,
+		.subhead-h,
+		.c9-sh,
+		.editor-styles-wrapper .c9-cta .c9-sh p,
+		.wp-block-table tr td,
+		.btn,
+		.btn:visited,
+		.entry-content button,
+		.entry-content input[type="button"],
+		.entry-content input[type="reset"],
+		.entry-content input[type="submit"],
+		.wp-block-button__link,
+		.wp-block-file__button,
+		.wp-block-file .wp-block-file__button,
+		#mc_embed_signup input[type="email"],
+		.c9 input[type="text"],
+		.c9 input[type="email"],
+		.c9 input[type="url"],
+		.c9 input[type="password"],
+		.c9 input[type="tel"],
+		.c9 textarea,
+		#fullscreensearch input[type="search"],
+		.c9 .gform_wrapper label.gfield_label,
+		.c9 .gform_wrapper legend.gfield_label,
+		.wp-block[data-type="gravityforms/block"] .gform_wrapper label.gfield_label,
+		.wp-block[data-type="gravityforms/block"] .gform_wrapper legend.gfield_label,
+		.c9 .gform_wrapper input[type="text"],
+		.c9 .gform_wrapper input[type="password"],
+		.c9 .gform_wrapper input[type="tel"],
+		.c9 .gform_wrapper textarea,
+		.wp-block[data-type="gravityforms/block"] .gform_wrapper input[type="text"],
+		.wp-block[data-type="gravityforms/block"] .gform_wrapper input[type="password"],
+		.wp-block[data-type="gravityforms/block"] .gform_wrapper input[type="tel"],
+		.wp-block[data-type="gravityforms/block"] .gform_wrapper textarea,
+		.c9 .gform_button.button,
+		.wp-block[data-type="gravityforms/block"] .gform_button.button,
 		.entry-content,
 		.editor-styles-wrapper p,
 		.editor-styles-wrapper .container p,
@@ -89,11 +133,8 @@ class C9FontStyles {
 		.editor-styles-wrapper .wp-block-paragraph,
 		.editor-styles-wrapper .wp-block-quote p,
 		.editor-styles-wrapper .wp-block-file,
-		.wp-block-pullquote,
-		.wp-block-pullquote blockquote p,
-		.editor-default-block-appender textarea.editor-default-block-appender__content,
-		#wrapper-footer {
-		font-family: <?php echo $body_font; ?>;
+		.editor-default-block-appender textarea.editor-default-block-appender__content {
+			font-family: <?php echo $body_font; ?>;
 		}
 		<?php
 	}
