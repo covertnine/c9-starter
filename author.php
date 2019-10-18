bbcb0de397a0ad632d826ab252b79a6c1a32b41b
COVERT NINE <tim@covertnine.com>
2019-10-17 21:30:09 -0500

    author page adjustments

--- author.php
+++ author.php
@@ -14,84 +14,74 @@ get_header();
 
 <div class="wrapper" id="author-wrapper">
 
-	<div class="container c9" id="content" tabindex="-1">
-
-		<div class="row">
-
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+	<div class="c9" id="content" tabindex="-1">
 
 			<main class="site-main" id="main">
 
-				<header class="page-header author-header">
-
-					<?php
-					$curauth = ( isset( $_GET['author_name'] ) ) ? get_user_by( 'slug', $author_name ) : get_userdata( intval( $author ) );
-					?>
-
-					<h1><?php esc_html_e( 'About:', 'c9' ); ?><?php echo esc_html( $curauth->nickname ); ?></h1>
-
-					<?php if ( ! empty( $curauth->ID ) ) : ?>
-						<?php echo get_avatar( $curauth->ID ); ?>
-					<?php endif; ?>
-
-					<dl>
-						<?php if ( ! empty( $curauth->user_url ) ) : ?>
-							<dt><?php esc_html_e( 'Website', 'c9' ); ?></dt>
-							<dd>
-								<a href="<?php echo esc_url( $curauth->user_url ); ?>"><?php echo esc_html( $curauth->user_url ); ?></a>
-							</dd>
+				<header class="page-header author-header entry-header">
+					<div class="container-narrow">
+						<div class="row mar20B p-5">
+						<?php
+						$curauth = ( isset( $_GET['author_name'] ) ) ? get_user_by( 'slug', $author_name ) : get_userdata( intval( $author ) );
+						?>
+						<?php if ( ! empty( $curauth->ID ) ) : ?>
+						<div class="col-xs-12 col-sm-4">
+							<?php echo get_avatar( $curauth->ID, 512 ); ?>
+						</div>
 						<?php endif; ?>
+						<div class="col-xs-12 col-sm-8">
 
-						<?php if ( ! empty( $curauth->user_description ) ) : ?>
-							<dt><?php esc_html_e( 'Profile', 'c9' ); ?></dt>
-							<dd><?php echo esc_html( $curauth->user_description ); ?></dd>
-						<?php endif; ?>
-					</dl>
+							<h1><?php esc_html_e( 'About: ', 'c9' ); ?><?php echo esc_html( $curauth->nickname ); ?></h1>
 
-					<h2><?php esc_html_e( 'Posts by', 'c9' ); ?> <?php echo esc_html( $curauth->nickname ); ?>:</h2>
+							<dl>
+								<?php if ( ! empty( $curauth->user_url ) ) : ?>
+									<dt><?php esc_html_e( 'Website', 'c9' ); ?></dt>
+									<dd>
+										<a href="<?php echo esc_url( $curauth->user_url ); ?>"><?php echo esc_html( $curauth->user_url ); ?></a>
+									</dd>
+								<?php endif; ?>
 
+								<?php if ( ! empty( $curauth->user_description ) ) : ?>
+									<dt><?php esc_html_e( 'Profile', 'c9' ); ?></dt>
+									<dd><?php echo esc_html( $curauth->user_description ); ?></dd>
+								<?php endif; ?>
+							</dl>
+						
+						</div><!--.col-->
+</div>
+					</div><!--.container-narrow-->
 				</header><!-- .page-header -->
+				<div class="container-narrow">
+					<div class="row pl-5">
+						<div class="col">
+								<h2><?php esc_html_e( 'Posts by', 'c9' ); ?> <?php echo esc_html( $curauth->nickname ); ?></h2>
+						</div>
+					</div>
+				</div>
+				<div class="container-narrow">
+					<div class="row mar30B p-5">
 
-				<ul>
-					<!-- The Loop -->
-					<?php
-					if ( have_posts() ) :
-						while ( have_posts() ) :
-							the_post();
-							?>
-							<li>
-								<a rel="bookmark" href="<?php the_permalink(); ?>" title="<?php esc_html_e( 'Permanent Link:', 'c9' ); ?> <?php the_title(); ?>">
-									<?php the_title(); ?></a>,
-								<?php
-								c9_posted_on();
-								esc_html_e( 'in', 'c9' );
-								the_category( '&' );
+				<?php if ( have_posts() ) : ?>
+							<?php
+							while ( have_posts() ) :
+								the_post();
+								get_template_part( 'loop-templates/content', get_post_format() );
+							endwhile;
 								?>
-							</li>
-						<?php endwhile; ?>
-
-					<?php else : ?>
-
-						<?php get_template_part( 'loop-templates/content', 'none' ); ?>
-
-					<?php endif; ?>
-
-					<!-- End Loop -->
-
-				</ul>
+						<?php
+						else :
+								get_template_part( 'loop-templates/content', 'none' );
+						endif;
+						?>
+					</div><!--.row-->
+				</div><!--.container-->
 
 			</main><!-- #main -->
 
 			<!-- The pagination component -->
 			<?php c9_pagination(); ?>
 
-		<!-- Do the right sidebar check -->
-		<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
-
-	</div> <!-- .row -->
-
-</div><!-- Container end -->
+</div><!-- .c9 end -->
 
 </div><!-- Wrapper end -->
 
