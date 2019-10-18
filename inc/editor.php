65d33df5c529594ee856e4961fa68dd9d11b6ab3
samirillion <sam@protonmail.com>
2019-10-18 16:55:20 -0500

    fixing another relative uri function

--- inc/editor.php
+++ inc/editor.php
@@ -70,7 +70,7 @@ if ( ! function_exists( 'c9_tiny_mce_before_init' ) ) {
 
 /* add theme compiled files to gutenberg editor */
 function c9_editor_style() {
-	wp_enqueue_style( 'c9-styles', get_stylesheet_directory_uri() . '/assets/dist/css/theme.min.css' );
+	wp_enqueue_style( 'c9-styles', get_template_directory_uri() . '/assets/dist/css/theme.min.css' );
 	wp_enqueue_style( 'c9-editor-style', get_template_directory_uri() . '/assets/dist/css/custom-editor-style.css' );
 	wp_enqueue_script( 'c9-scripts-theme', get_template_directory_uri() . '/assets/dist/js/theme.min.js' );
 }
