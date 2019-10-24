4ee799e934c68fc142bc09af197605c8dd6bda74
COVERT NINE <tim@covertnine.com>
2019-10-24 16:10:03 -0500

    added google maps suppor to magnific buttons

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -86,7 +86,7 @@ jQuery(document).ready(function() {
     ///////////////////////// for putting WordPress galleries linked to images/videos in lightbox ////////////////////
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     $(
-      ".cortex-popup-video,a.wp-block-button__link[href*='youtube.com'],a.wp-block-button__link[href*='vimeo.com']"
+      ".cortex-popup-video,a.wp-block-button__link[href*='youtube.com'],a.wp-block-button__link[href*='vimeo.com'],a.wp-block-button__link[href*='maps.google.com']"
     ).magnificPopup({
       disableOn: 700,
       type: "iframe",
