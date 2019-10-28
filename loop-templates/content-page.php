7ae6a18bae9c571aa5de5ca599e9655287d5997e
COVERT NINE <tim@covertnine.com>
2019-10-28 11:56:01 -0500

    removing footer empty on content-page, font adjustments and list item adjustments

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -25,10 +25,4 @@
 
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-
-		<?php edit_post_link( __( 'Edit', 'c9' ), '<span class="edit-link">', '</span>' ); ?>
-
-	</footer><!-- .entry-footer -->
-
 </article><!-- #post-## -->
