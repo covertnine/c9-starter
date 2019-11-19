af7d8c73590e0cb472c36ed175e7515c78904e7e
COVERT NINE <tim@covertnine.com>
2019-11-19 17:48:58 -0600

    removing dev domain adding js classes for linting message in js removed duplicate bootstrap inclusion in editor style removing old theme code for tinymce

--- inc/editor.php
+++ inc/editor.php
@@ -9,65 +9,6 @@
 /**
  * Registers an editor stylesheet for the theme.
  */
-// Adding theme support
-add_theme_support( 'editor-styles' );
-
-// Add TinyMCE style formats.
-add_filter( 'mce_buttons_2', 'c9_tiny_mce_style_formats' );
-
-if ( ! function_exists( 'c9_tiny_mce_style_formats' ) ) {
-	function c9_tiny_mce_style_formats( $styles ) {
-
-		array_unshift( $styles, 'styleselect' );
-		return $styles;
-	}
-}
-
-
-add_filter( 'tiny_mce_before_init', 'c9_tiny_mce_before_init' );
-
-if ( ! function_exists( 'c9_tiny_mce_before_init' ) ) {
-	function c9_tiny_mce_before_init( $settings ) {
-
-		$style_formats = array(
-			array(
-				'title'    => 'Lead Paragraph',
-				'selector' => 'p',
-				'classes'  => 'lead',
-				'wrapper'  => true,
-			),
-			array(
-				'title'  => 'Small',
-				'inline' => 'small',
-			),
-			array(
-				'title'   => 'Blockquote',
-				'block'   => 'blockquote',
-				'classes' => 'blockquote',
-				'wrapper' => true,
-			),
-			array(
-				'title'   => 'Blockquote Footer',
-				'block'   => 'footer',
-				'classes' => 'blockquote-footer',
-				'wrapper' => true,
-			),
-			array(
-				'title'  => 'Cite',
-				'inline' => 'cite',
-			),
-		);
-
-		if ( isset( $settings['style_formats'] ) ) {
-			$orig_style_formats = json_decode( $settings['style_formats'], true );
-			$style_formats      = array_merge( $orig_style_formats, $style_formats );
-		}
-
-		$settings['style_formats'] = json_encode( $style_formats );
-		return $settings;
-	}
-}
-
 /* add theme compiled files to gutenberg editor */
 function c9_editor_style() {
 	wp_enqueue_style( 'c9-styles', get_template_directory_uri() . '/assets/dist/css/theme.min.css' );
@@ -76,7 +17,6 @@ function c9_editor_style() {
 }
 
 add_action( 'enqueue_block_editor_assets', 'c9_editor_style' );
-
 
 /* add page template name to body_class in admin */
 if ( ! function_exists( 'c9_template_selected' ) ) {
