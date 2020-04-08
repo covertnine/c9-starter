5ec3a5b52bb994bc26e64f7be6d1cbf708de4776
tim <tim@covertnine.com>
2020-04-08 16:32:28 -0500

    removing customizer settings and customizer settings

--- inc/customizer.php
+++ inc/customizer.php
@@ -21,6 +21,13 @@ if ( ! function_exists( 'c9_customize_register' ) ) {
 		$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 		$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 		$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+		// Add Footer Section
+// $wp_customize-&amp;gt;add_section('your_theme_footer', array(
+// 'title' =&amp;gt; 'Footer',
+// 'description' =&amp;gt; '',
+// 'priority' =&amp;gt; 120,
+// ));
 	}
 }
 add_action( 'customize_register', 'c9_customize_register' );
