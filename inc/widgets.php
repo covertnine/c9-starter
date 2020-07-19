71a8fadb311fdc240f122db374380046900e5351
tim <tim@covertnine.com>
2020-07-19 11:57:16 -0500

    default widget content now showing up on fresh install

--- inc/widgets.php
+++ inc/widgets.php
@@ -66,7 +66,7 @@ if ( ! function_exists( 'c9_widgets_init' ) ) {
 				'name'          => __( 'Footer', 'c9-work' ),
 				'id'            => 'footerfull',
 				'description'   => 'Full bottom widget with dynamic responsive grid that shows on every page. Suggested 3-6 widgets.',
-				'before_widget' => '<div id="%1$s" class="footer-widget %2$s' . c9_slbd_count_widgets( 'footerfull' ) . '">',
+				'before_widget' => '<div id="%1$s" class="footer-widget %2$s ' . c9_slbd_count_widgets( 'footerfull' ) . '">',
 				'after_widget'  => '</div><!-- .footer-widget -->',
 				'before_title'  => '<h3 class="widget-title">',
 				'after_title'   => '</h3>',
