abc8e57f31e300ab20b66bea3d74cacda48702ce
tim <tim@covertnine.com>
2020-08-25 15:35:03 -0400

    removing non-necessary plugins

--- admin/tgm-init.php
+++ admin/tgm-init.php
@@ -72,24 +72,6 @@ function c9_register_required_plugins() {
 		),
 
 		array(
-			'name'     => 'The SEO Framework',
-			'slug'     => 'autodescription',
-			'required' => false,
-		),
-
-		array(
-			'name'     => 'Autoptimize',
-			'slug'     => 'autoptimize',
-			'required' => false,
-		),
-
-		array(
-			'name'     => 'Matomo Analytics',
-			'slug'     => 'matomo',
-			'required' => false,
-		),
-
-		array(
 			'name'     => 'Regenerate Thumbnails',
 			'slug'     => 'regenerate-thumbnails',
 			'required' => false,
@@ -99,12 +81,6 @@ function c9_register_required_plugins() {
 			'name'     => 'WP Retina 2x',
 			'slug'     => 'wp-retina-2x',
 			'required' => false,
-		),
-
-		array(
-			'name'		=> 'Smash Balloon Social Photo Feed',
-			'slug'		=> 'instagram-feed',
-			'required'	=> false,
 		),
 
 	);
