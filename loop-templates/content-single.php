390bad64eb56d0a8a76205bdbd425c94d8c9c20b
tim <tim@covertnine.com>
2020-12-01 19:14:13 -0600

    fix one translation string

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -112,7 +112,7 @@ if ($c9_blog_sidebar  != 'hide') {
 			<?php
 			wp_link_pages(
 				array(
-					'before' => '<div class="page-links">' . __('Pages:', 'c9-starter'),
+					'before' => '<div class="page-links">' . __('Pages:', 'c9-work'),
 					'after'  => '</div>',
 				)
 			);
