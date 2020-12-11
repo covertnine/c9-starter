a15bec354fc47e6f79ade82ecea6a55dcc83671c
Dylan Krepps <dskrepps@gmail.com>
2020-12-11 21:36:25 +0000

    Fix post header size small by default

--- admin/admin-settings.php
+++ admin/admin-settings.php
@@ -34,7 +34,7 @@ add_action('add_meta_boxes', 'c9_post_header_size');
 function c9_post_header_size_html($post)
 {
 	$c9_post_header_size = get_post_meta($post->ID, 'c9_post_header_size', true);
-	$value = isset($c9_post_header_size) ? $c9_post_header_size : 'small';
+	$value = !empty($c9_post_header_size) ? $c9_post_header_size : 'small';
 ?>
 	<label for="c9_post_header_size"><?php echo esc_html('Header Size', 'c9-togo'); ?></label>
 	<div>
