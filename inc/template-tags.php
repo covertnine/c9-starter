ceb57d23ad28d5e505b2e1a220a941e195bfd954
Dylan Krepps <dskrepps@gmail.com>
2020-12-11 21:51:03 +0000

    Typo in c9_posted_on resulting in hidden date

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -15,7 +15,7 @@
 if (!function_exists('c9_posted_on')) {
 	function c9_posted_on()
 	{
-		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
 		if (get_the_time('U') !== get_the_modified_time('U')) {
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s"> (%4$s) </time>';
 		}
