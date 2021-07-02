fb405e0c6b625a260eaf3795398be2637766fabf
Tim <tim@admins-MacBook-Pro.local>
2021-07-02 14:03:32 -0500

    changes to 404 error page

--- loop-templates/content-none.php
+++ loop-templates/content-none.php
@@ -15,7 +15,10 @@
 
 			<header class="page-header">
 
-				<h1 class="page-title mar30T"><?php esc_html_e('Nothing Found', 'c9-starter'); ?></h1>
+				<h1 class="page404-title mar30T"><?php esc_html_e('Oops!', 'c9-starter'); ?></h1>
+				<h2 class="page404-subtitle h1 text-xl">
+					<?php esc_html_e('there\'s nothing here.', 'c9-starter'); ?>
+				</h2>
 
 			</header><!-- .page-header -->
 
@@ -44,13 +47,13 @@
 				elseif (is_search()) :
 				?>
 
-					<p><?php esc_html_e('Sorry, but nothing matched your search terms.', 'c9-starter'); ?> Try searching, or use our <a href="/locations" title="Find a Wow Bao near you!">Locations</a> link to search by address or ZIP code.
+					<p>Try searching below, or search by zip code on our <a href="/locations" title="Find a Wow Bao near you!">Locations</a> page.
 					</p>
 				<?php
 					get_search_form();
 				else :
 				?>
-					<p><?php esc_html_e('It seems we can&rsquo;t find what you&rsquo;re looking for.', 'c9-starter'); ?> Try searching, or use our <a href="/locations" title="Find a Wow Bao near you!">Locations</a> link to search by address or ZIP code.
+					<p><?php esc_html_e('It seems we can&rsquo;t find what you&rsquo;re looking for.', 'c9-starter'); ?> <br />Try searching, or use our <a href="/locations" title="Find a Wow Bao near you!">Locations</a> link to search by address or ZIP code.
 					</p>
 				<?php
 					get_search_form();
