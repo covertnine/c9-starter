5b2d259154bc9e4e9c9ee81889de5491eb93069b
COVERT NINE <covertnine@users.noreply.github.com>
2022-05-11 08:32:02 -0500

    changing tickets link

--- header.php
+++ header.php
@@ -60,7 +60,7 @@ $c9_header_search = get_theme_mod('c9_header_hide_search', false);
 							<div class="navbar-small-buttons">
 								<div class="header-buttons-mobile d-inline-block d-sm-none">
 									<!-- <a href="/chicago/tickets" title="Riot Fest Chicago Tickets" class="btn btn-xs has-color-secondary-background-color">Tickets</a> -->
-									<a href="/chicago/tickets" title="Riot Fest Chicago Lineup" class="btn btn-xs has-color-success-background-color">Tickets</a>
+									<a href="https://www.eventbrite.com/o/riot-fest-17254529109" title="Riot Fest Chicago Tickets" class="btn btn-xs has-color-success-background-color">Tickets</a>
 								</div>
 								<?php if (!$c9_header_search) { ?>
 									<div class="nav-search">
