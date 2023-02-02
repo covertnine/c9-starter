b1f6a11e3689274fe9cb1cff7413654d3b205049
tim <tim@covertnine.com>
2023-02-02 17:33:43 -0600

    change to viewport meta

--- header.php
+++ header.php
@@ -13,7 +13,6 @@ $c9_header_search = get_theme_mod('c9_header_hide_search', false);
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
