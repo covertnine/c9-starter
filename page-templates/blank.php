342ae4d8ffde7a10f90d06e4f509381aacd76a51
tim <tim@covertnine.com>
2023-02-02 17:47:29 -0600

    viewport width adjustment

--- page-templates/blank.php
+++ page-templates/blank.php
@@ -15,7 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">
+	<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
