d54cb30720247d557db83dddf7d5d44ae73866de
tim <tim@covertnine.com>
2024-03-10 11:44:11 -0400

    adding patterns to folder for modal dialog

---
+++ patterns/events-schedule-core.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Title: Event Tour Schedule
+ * Slug: c9-starter/events-schedule-core
+ * Categories: page
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page
+ * Viewport width: 1240
+ */
+
+?>
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/client/client-assets/img/widescreen-homepage-hero-1920x1200-v2.jpg","id":1895,"dimRatio":30,"overlayColor":"c9music-black","focalPoint":{"x":"0.49","y":"0.67"},"minHeight":849,"minHeightUnit":"px","contentPosition":"bottom center","align":"full","className":"has-color-dark-background-color has-background-dim","c9ContainerWidth":"container-fluid","c9Align":"full"} -->
+<div class="wp-block-cover alignfull has-custom-content-position is-position-bottom-center has-color-dark-background-color has-background-dim alignfull container-fluid" style="min-height:849px"><span aria-hidden="true" class="wp-block-cover__background has-c-9-music-black-background-color has-background-dim-30 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-1895" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/client/client-assets/img/widescreen-homepage-hero-1920x1200-v2.jpg" style="object-position:49% 67%" data-object-fit="cover" data-object-position="49% 67%"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"66.67%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.67%"><!-- wp:image {"id":1955,"width":"66px","height":"auto","aspectRatio":"1","sizeSlug":"large","linkDestination":"none","className":"is-resized aligncenter"} -->
+<figure class="wp-block-image size-large is-resized aligncenter"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/client/client-assets/img/feather-logo-gradient-rb.svg" alt="" class="wp-image-1955" style="aspect-ratio:1;width:66px;height:auto"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":1,"className":"wp-block-heading has-text-align-center has-large-font-size"} -->
+<h1 class="wp-block-heading has-text-align-center has-large-font-size"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-color-light-color">Event – OF THE CENTURY</mark><br><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-color-light-color">told you we'd be back</mark></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"has-inline-color has-color-light-color has-text-align-center","fontSize":"small"} -->
+<p class="has-inline-color has-color-light-color has-text-align-center has-small-font-size">↓ SCROLL FOR THE DATES AND LISTEN TO THE JAMS WHILE YOU DO!</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:embed {"url":"https://open.spotify.com/album/1EFnZuLUzXx0B6ZsL9aqum?si=mGx9OSaiSJ2T2GlAojjwDw","type":"rich","providerNameSlug":"spotify","allowResponsive":false,"responsive":true,"className":""} -->
+<figure class="wp-block-embed is-type-rich is-provider-spotify wp-block-embed-spotify"><div class="wp-block-embed__wrapper">
+https://open.spotify.com/album/1EFnZuLUzXx0B6ZsL9aqum?si=mGx9OSaiSJ2T2GlAojjwDw
+</div></figure>
+<!-- /wp:embed --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
+
+
+<!-- wp:spacer {"height":"17px"} -->
+<div style="height:17px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"color":{"background":"#f7f7f7"}}} -->
+<div class="wp-block-group has-background" style="background-color:#f7f7f7"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"70%"} -->
+<div class="wp-block-column" style="flex-basis:70%"><!-- wp:heading {"level":1,"style":{"typography":{"fontSize":35}}} -->
+<h1 class="wp-block-heading" style="font-size:35px"><strong><span class="has-inline-color has-color-primary-color">C9</span></strong> — Events Block Pattern</h1>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"23px"} -->
+<div style="height:23px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<h3 class="wp-block-heading has-medium-font-size">COVERT NINE - The nine essentials ingredients for good digital marketing. Copywriting, Design, Development, SEO, PPC, Social Media, Email, Videos, Photograph</h3>
+<!-- /wp:heading -->
+
+<!-- wp:separator {"opacity":"css"} -->
+<hr class="wp-block-separator has-css-opacity"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph -->
+<p class=""><strong>Tickets and on-sale information would be great in here. </strong>Orci eu lobortis elementum nibh tellus molestie. Tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p class=""><strong>Frequently asked questions would go great in here.</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p class="">Orci eu lobortis elementum nibh tellus molestie. Tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p class=""><strong>All content editing is done inline. </strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p class="">Orci eu lobortis elementum nibh tellus molestie. Tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p class="">Have fun!</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">GET TICKETS</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">ASK TIRESOME QUESTIONS</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"60%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:60%"><!-- wp:table {"className":"is-style-stripes"} -->
+<figure class="wp-block-table is-style-stripes"><table><thead><tr><th>Date</th><th>City</th><th class="has-text-align-center" data-align="center">Tickets</th></tr></thead><tbody><tr><td>6-7-2021</td><td>Chicago, IL</td><td class="has-text-align-center" data-align="center"><a href="#">Buy Tickets</a></td></tr><tr><td>6-8-2021</td><td>Chicago, IL</td><td class="has-text-align-center" data-align="center"><a href="#">Buy Tickets</a></td></tr><tr><td>6-14-2021</td><td>Savannah, GA</td><td class="has-text-align-center" data-align="center"><a href="#">Buy Tickets</a></td></tr><tr><td>6-18-2021</td><td>Los Angeles, CA</td><td class="has-text-align-center" data-align="center"><a href="#">Buy Tickets</a></td></tr><tr><td>7-1-2021</td><td>New York City!</td><td class="has-text-align-center" data-align="center"><a href="#">Buy Tickets</a></td></tr><tr><td>7-12-2021</td><td>Paris, France</td><td class="has-text-align-center" data-align="center"><a href="#">Buy Tickets</a></td></tr><tr><td>7-18-2021</td><td>Washington DC</td><td class="has-text-align-center" data-align="center"><a href="#">Buy Tickets</a></td></tr><tr><td>7-31-2021</td><td>Chicago, IL</td><td class="has-text-align-center" data-align="center">Coming soon!</td></tr><tr><td>8-2-2021</td><td>Milwaukee, WI</td><td class="has-text-align-center" data-align="center">Coming soon!</td></tr><tr><td>8-4-2021</td><td>Portland, OR</td><td class="has-text-align-center" data-align="center">Coming soon!</td></tr><tr><td>8-6-2021</td><td>Seattle, WA</td><td class="has-text-align-center" data-align="center">Coming soon!</td></tr></tbody></table></figure>
+<!-- /wp:table --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
