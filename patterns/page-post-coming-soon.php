e499f5b6561fe809e5dd2666aedee10a2fc52645
tim <tim@covertnine.com>
2024-03-29 19:00:45 -0500

    adding new version and fixing viewport widtrh

--- patterns/page-post-coming-soon.php
+++ patterns/page-post-coming-soon.php
@@ -6,7 +6,7 @@
  * Keywords: starter
  * Block Types: core/post-content
  * Post Types: page, post
- * Viewport width: 1200
+ * Viewport width: 900
  */
 ?>
 <!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/client/client-assets/img/build-burger-ingredients.jpg","id":2789,"dimRatio":80,"overlayColor":"color-primary","minHeight":995,"minHeightUnit":"px","align":"full"} -->
